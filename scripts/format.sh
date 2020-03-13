0dcf411d13acdb5c4533c43e638de714c616fb57
Kazuya Izumi <shikibu9419@gmail.com>
2020-03-13 19:03:35 +0900

    Add .devcontainer

--- scripts/format.sh
+++ scripts/format.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
-rootfile=$1
+rootfile=${1:-document.tex}
 
 find sections -type f -name '*.tex' -print0 | \
   xargs -0 sed -i -e 's/、/，/g' -e 's/。/．/g'
 sed -i -e 's/、/，/g' -e 's/。/．/g' $rootfile
+
+echo 'format.sh: formatted.'
